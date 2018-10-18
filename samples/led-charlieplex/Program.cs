5290e381e6f8be8c277282f6e79b92705801eafa
Rich Lander <rlander@microsoft.com>
2018-10-17 22:24:05 -0700

    Add pin switching

--- samples/led-charlieplex/Program.cs
+++ samples/led-charlieplex/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Devices.Gpio;
+using System.Threading;
 
 namespace led_charlieplex
 {
@@ -10,6 +11,34 @@ namespace led_charlieplex
             Console.WriteLine("Hello World!");
 
             var controller = new GpioController(PinNumberingScheme.Gpio);
+
+            var pinA = controller.OpenPin(4,PinMode.Output);
+            var pinB = controller.OpenPin(5, PinMode.Output);
+            var pinC = controller.OpenPin(6, PinMode.Input);
+
+            while (true)
+            {
+                // led 1
+                Console.WriteLine("led 1");
+                pinA.Mode = PinMode.Output;
+                pinC.Mode = PinMode.Input;
+                pinA.Write(PinValue.High);
+                pinB.Write(PinValue.Low);
+                Thread.Sleep(500);
+                // led 2
+                Console.WriteLine("led 2");
+                pinA.Write(PinValue.Low);
+                pinB.Write(PinValue.High);
+                Thread.Sleep(500);
+                // led 3
+                Console.WriteLine("led 3");
+                pinA.Write(PinValue.Low);
+                pinA.Mode = PinMode.Input;
+                pinC.Mode = PinMode.Output;
+                pinB.Write(PinValue.High);
+                pinC.Write(PinValue.Low);
+                Thread.Sleep(500);
+            }
         }
     }
 }
---
+++ samples/led-charlieplex/pub.bat
@@ -0,0 +1,2 @@
+dotnet build
+scp bin\Debug\netcoreapp2.1\led-charlieplex.dll pi@192.168.1.76:~/led-charlieplex/bin/Debug/netcoreapp2.1
