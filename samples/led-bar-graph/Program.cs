31e700e2c1825ad7f7dd8e0a04e69d102b4af373
Rich Lander <rlander@microsoft.com>
2019-06-30 22:01:06 -0700

    Switch AnimateLeds to object

--- samples/led-bar-graph/Program.cs
+++ samples/led-bar-graph/Program.cs
@@ -12,33 +12,34 @@ namespace led_bar_graph
             Console.WriteLine("Hello World!");
 
             var pins = new int[] {4,5,6,12,13,16,17,18,19,20};
-
+            var cancellationSource = new CancellationTokenSource();
             using var controller = new GpioController();
-            foreach(var pin in pins)
-            {
-                controller.OpenPin(pin, PinMode.Output);
-            }
+            var leds = new AnimateLeds(controller);
+            leds.Cancellation = cancellationSource.Token;
+            leds.Init(pins);
             Console.CancelKeyPress += (s, e) => 
             { 
                 e.Cancel = true;
+                cancellationSource.Cancel();
+                Thread.Sleep(50);
+                var leds2 = new AnimateLeds(controller);
+                leds2.DimAllAtRandom(20, pins);
             };
                       
             var litTime = 200;
             var dimTime = 50;
-            AnimateLed.Controller = controller;
-controller.Write(6,PinValue.High);
             Console.WriteLine($"Animate! {pins.Length} pins in use.");
 
-            while (AnimateLed.Cancellation.IsCancellationRequested)
+            while (leds.Cancellation.IsCancellationRequested)
             {
                 Console.WriteLine($"Lit: {litTime}ms; Dim: {dimTime}");
-                AnimateLed.FrontToBack(litTime,dimTime,pins,true);
-                AnimateLed.BacktoFront(litTime, dimTime, pins);
-                AnimateLed.MidToEnd(litTime,dimTime,pins);
-                AnimateLed.EndToMid(litTime, dimTime, pins);
-                AnimateLed.MidToEnd(litTime, dimTime, pins);
-                AnimateLed.LightAll(litTime,dimTime,pins);
-                AnimateLed.DimAllAtRandom(litTime, dimTime, pins);
+                leds.FrontToBack(litTime,dimTime,pins,true);
+                leds.BacktoFront(litTime, dimTime, pins);
+                leds.MidToEnd(litTime,dimTime,pins);
+                leds.EndToMid(litTime, dimTime, pins);
+                leds.MidToEnd(litTime, dimTime, pins);
+                leds.LightAll(litTime,dimTime,pins);
+                leds.DimAllAtRandom(dimTime, pins);
 
                 if (litTime < 20)
                 {
